407b7167da53082c5ff67baffd3250b40c6a45e3
julian david <jvelandian@unal.edu.co>
2025-07-23 08:45:37 -0500

    Es posible incluir valores repetidos del PBM de estudiantes

--- src/main/java/com/proyecto/estructuras/models/AVLHashMap.java
+++ src/main/java/com/proyecto/estructuras/models/AVLHashMap.java
@@ -124,14 +124,9 @@ public class AVLHashMap<K, V extends Comparable<V>> {
 
         if (node.data.compareTo(data) > 0)
             node.left = insertRec(node.left, data, node, key);
-
-        else if (node.data.compareTo(data) < 0)
+        //Se incluye los estudiantes con PBM repetido
+        else
             node.right = insertRec(node.right, data, node, key);
-
-        else {
-            System.out.println("El valor " + data + " ya existe en el árbol");
-            return node;
-        }
         return rebalance(node, data, true);
     }
     
